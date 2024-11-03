84863c37df4590494978ffc53c5ccb22f550bb78
Ëinari Mäkiranta <makirantaeinari@gmail.com>
2024-11-03 22:36:29 +0200

    Changes/improvements to simple::Set and simple::DynamicArray

--- simple/headers/simple_dynamic_array.hpp
+++ simple/headers/simple_dynamic_array.hpp
@@ -19,9 +19,9 @@ namespace simple {
 			if (other._capacity == 0) {
 				return;
 			}
-			_pData = getAllocator().allocate(_capacity);
+			_pData = GetAllocator().allocate(_capacity);
 			for (size_t i = 0; i < other._size; i++) {
-				getAllocator().construct(&_pData[i], other._pData[i]);
+				GetAllocator().construct(&_pData[i], other._pData[i]);
 			}
 		}
 
@@ -38,29 +38,29 @@ namespace simple {
 			while (_capacity < _size) {
 				_capacity *= 2;
 			}
-			_pData = getAllocator().allocate(_capacity);
+			_pData = GetAllocator().allocate(_capacity);
 			for (uint32_t i = 0; i < _size; i++) {
-				getAllocator().construct(&_pData[i]);
+				GetAllocator().construct(&_pData[i]);
 			}
 		}
 
-		constexpr inline Allocator getAllocator() noexcept {
+		constexpr inline Allocator GetAllocator() noexcept {
 			return Allocator();
 		}
 
-		constexpr inline size_t capacity() const noexcept {
+		constexpr inline size_t Capacity() const noexcept {
 			return _capacity;
 		}
 
-		constexpr inline size_t size() const noexcept {
+		constexpr inline size_t Size() const noexcept {
 			return _size;
 		}
 
-		constexpr inline T* data() const noexcept {
+		constexpr inline T* Data() const noexcept {
 			return _pData;
 		}
 
-		inline DynamicArray& reserve(uint32_t capacity) {
+		inline DynamicArray& Reserve(uint32_t capacity) {
 			if (capacity <= _capacity || capacity == 0) {
 				return *this;
 			}
@@ -68,39 +68,39 @@ namespace simple {
 			while (_capacity < capacity) {
 				_capacity *= 2;
 			}
-			T* temp = getAllocator().allocate(_capacity);
+			T* temp = GetAllocator().allocate(_capacity);
 			assert(temp && "failed to allocate memory!");
 			for (size_t i = 0; i < _size; i++) {
-				getAllocator().construct(&temp[i], std::move(_pData[i]));
+				GetAllocator().construct(&temp[i], std::move(_pData[i]));
 			}
-			getAllocator().deallocate(_pData, 1);
+			GetAllocator().deallocate(_pData, 1);
 			_pData = temp;
 			return *this;
 		}
 
-		inline DynamicArray& resize(uint32_t size) {
+		inline DynamicArray& Resize(uint32_t size) {
 			uint32_t newCapacity = _capacity ? _capacity : 1;
 			while (size > newCapacity) {
 				newCapacity *= 2;
 			}
-			reserve(newCapacity);
+			Reserve(newCapacity);
 			_size = size;
 			for (size_t i = 0; i < _size; i++) {
-				getAllocator().construct(&_pData[i]);
+				GetAllocator().construct(&_pData[i]);
 			}
 			return *this;
 		}
 
-		inline Iterator pushBack(const T& value) {
+		inline Iterator PushBack(const T& value) {
 			if (_size >= _capacity) {
 				reserve((_capacity ? _capacity : 1) * 2);
 			}
-			getAllocator().construct(&_pData[_size]);
+			GetAllocator().construct(&_pData[_size]);
 			_pData[_size++] = value;
-			return back();
+			return Back();
 		}
 
-		inline Iterator insert(Iterator where, const T& value) {
+		inline Iterator Insert(Iterator where, const T& value) {
 			if (where == &_pData[_size]) {
 				return pushBack(value);
 			}
@@ -108,24 +108,24 @@ namespace simple {
 				reserve((_capacity ? _capacity : 1) * 2);
 			}
 			for (auto iter = &_pData[_size]; iter != where; iter--) {
-				getAllocator().construct(iter, std::move(*(iter - 1)));
+				GetAllocator().construct(iter, std::move(*(iter - 1)));
 			}
-			getAllocator().construct(where, value);
+			GetAllocator().construct(where, value);
 			++_size;
 			return where;
 		}
 
 		template<typename... Args>
-		inline Iterator emplaceBack(Args&&... args) {
+		inline Iterator EmplaceBack(Args&&... args) {
 			if (_size >= _capacity) {
 				reserve((_capacity ? _capacity : 1) * 2);
 			}
-			getAllocator().construct(&_pData[_size++], std::forward<Args>(args)...);
+			GetAllocator().construct(&_pData[_size++], std::forward<Args>(args)...);
 			return back();
 		}
 
 		template<typename... Args>
-		inline Iterator emplace(Iterator where, Args&&... args) {
+		inline Iterator Emplace(Iterator where, Args&&... args) {
 			if (where == &_pData[_size]) {
 				return emplaceBack(std::forward<Args>(args)...);
 			}
@@ -133,39 +133,52 @@ namespace simple {
 				reserve((_capacity ? _capacity : 1) * 2);
 			}
 			for (auto iter = &_pData[_size]; iter != where; iter--) {
-				getAllocator().construct(iter, std::move(*(iter - 1)));
+				GetAllocator().construct(iter, std::move(*(iter - 1)));
 			}
-			getAllocator().construct(where, std::forward<Args>(args)...);
+			GetAllocator().construct(where, std::forward<Args>(args)...);
 			++_size;
 			return where;
 		}
 
-		inline Iterator erase(Iterator where) {
+		inline Iterator Erase(Iterator where) {
 			Iterator iter = where;
 			size_t index = iter - &_pData[0];
 			if (iter == &_pData[_size]) {
 			}
 			while (iter != &_pData[_size - 1]) {
-				getAllocator().destroy(iter);
-				getAllocator().construct(iter, std::move(*(iter + 1)));
+				GetAllocator().destroy(iter);
+				GetAllocator().construct(iter, std::move(*(iter + 1)));
 				++iter;
 			}
 			--_size;
 			return &_pData[index];
 		}
 
-		inline Iterator back() {
+		inline Iterator Back() {
 			return &_pData[_size - 1];
 		}
 
-		inline void clear() {
+		inline void Clear() {
 			for (size_t i = 0; i < _size; i++) {
-				getAllocator().destroy(&_pData[i]);
+				GetAllocator().destroy(&_pData[i]);
 			}
 			_capacity = 0;
 			_size = 0;
-			getAllocator().deallocate(_pData, 1);
+			GetAllocator().deallocate(_pData, 1);
 			_pData = nullptr;
+		}
+
+		inline DynamicArray& Reverse() {
+			if (!_size) {
+				return *this;
+			}
+			for (uint32_t i = 0, j = _size - 1; i != j && i < j;) {
+				T temp(std::move(_pData[i]));
+				GetAllocator().construct(&_pData[i], std::move(_pData[j]));
+				GetAllocator().construct(&_pData[j], std::move(temp));
+				++i; --j;
+			}
+			return *this;
 		}
 
 		inline Iterator begin() const {
@@ -176,26 +189,13 @@ namespace simple {
 			return &_pData[_size];
 		}
 
-		inline DynamicArray& reverse() {
-			if (!_size) {
-				return *this;
-			}
-			for (uint32_t i = 0, j = _size - 1; i != j && i < j;) {
-				T temp(std::move(_pData[i]));
-				getAllocator().construct(&_pData[i], std::move(_pData[j]));
-				getAllocator().construct(&_pData[j], std::move(temp));
-				++i; --j;
-			}
-			return *this;
-		}
-
 		~DynamicArray() noexcept {
 			for (size_t i = 0; i < _size; i++) {
-				getAllocator().destroy(&_pData[i]);
+				GetAllocator().destroy(&_pData[i]);
 			}
 			_capacity = 0;
 			_size = 0;
-			getAllocator().deallocate(_pData, 1);
+			GetAllocator().deallocate(_pData, 1);
 			_pData = nullptr;
 		}
 
@@ -207,9 +207,9 @@ namespace simple {
 		DynamicArray& operator=(const DynamicArray& other) {
 			_capacity = other._capacity;
 			_size = other._size;
-			_pData = getAllocator().allocate(_capacity);
+			_pData = GetAllocator().allocate(_capacity);
 			for (uint32_t i = 0; i < _size; i++) {
-				getAllocator().construct(&_pData[i], other._pData[i]);
+				GetAllocator().construct(&_pData[i], other._pData[i]);
 			}
 			return *this;
 		}	
--- simple/headers/simple_set.hpp
+++ simple/headers/simple_set.hpp
@@ -3,14 +3,15 @@
 #include "simple_allocator.hpp"
 #include "simple_array.hpp"
 #include "simple_dynamic_array.hpp"
-#include "tuple.hpp"
-#include "engine_logger.hpp"
+#include "simple_tuple.hpp"
+#include "simple_logging.hpp"
+#include <cstdint>
+#include <utility>
 
 namespace simple {
 
 	template<typename T, typename Hasher, uint8_t MaxBucketSize = 4,
-		typename ElementAllocator = DynamicAllocator<T>, 
-		typename BucketAllocator = DynamicAllocator<Tuple<Array<Tuple<T, bool>, MaxBucketSize>, uint8_t>>>
+		typename Allocator = DynamicAllocator<Tuple<Array<Tuple<T, bool>, MaxBucketSize>, uint8_t>>>
 	class Set {
 	public:
 
@@ -56,11 +57,11 @@ namespace simple {
 		};
 
 		inline Set() : _capacity(128), _buckets(nullptr), _elements(), _trash(0) {
-			_buckets = getBucketAllocator().allocate(_capacity);
+			_buckets = Allocator().allocate(_capacity);
 			for (size_t i = 0; i < _capacity; i++) {
-				getBucketAllocator().construct(&_buckets[i]);
+				Allocator().construct(&_buckets[i]);
 			}
-			_elements.reserve(_capacity);
+			_elements.Reserve(_capacity);
 		}
 
 		inline Set(Set&& other) noexcept
@@ -69,19 +70,11 @@ namespace simple {
 			other._capacity = 0;
 			other._buckets = nullptr;
 		}
-		
-		constexpr inline ElementAllocator getValueAllocator() noexcept {
-			return ElementAllocator();
-		}
 
-		constexpr inline BucketAllocator getBucketAllocator() noexcept {
-			return BucketAllocator();
-		}
-
-		void cleanup() {
+		void Cleanup() {
 			for (auto iter = _elements.begin(); iter != _elements.end();) {
 				if (!(*iter)->second) {
-					iter = _elements.erase(iter);
+					iter = _elements.Erase(iter);
 					continue;
 				}
 				++iter;
@@ -89,10 +82,30 @@ namespace simple {
 			_trash = 0;
 		}
 
-		inline Tuple<bool, T*> insert(const T& value) {
-			if (_trash > 8) {
-				cleanup();
+		inline void Reserve(uint32_t capacity) {
+			if (capacity <= _capacity) {
+				return;
 			}
+			uint32_t oldCapacity = _capacity;
+			while (capacity > _capacity) {
+				_capacity *= 2;
+			}
+			Bucket* temp = _buckets;
+			_buckets = Allocator().allocate(_capacity);
+			for (size_t i = 0; i < _capacity; i++) {
+				Allocator().construct(&_buckets[i]);
+			}
+			_elements.clear();
+			_elements.reserve(_capacity);
+			for (size_t i = 0; i < oldCapacity; i++) {
+				for (size_t j = 0; j < temp[i].second; j++) {
+					Emplace(std::move(temp[i].first[j].first));
+				}
+			}
+			Allocator().deallocate(temp, 1);
+		}
+
+		inline Tuple<bool, T*> Insert(const T& value) {
 			uint64_t hash = Hasher()(value);
 			auto& bucket = _buckets[hash % _capacity];
 			if (bucket.second) {
@@ -101,25 +114,22 @@ namespace simple {
 						return { false, nullptr };
 					}
 				}
-				simple_engine::logMessage("bad hash!");
+				simple::logMessage(this, "bad hash (function simple::Set::Insert)!");
 				if (bucket.second >= MaxBucketSize - 1) {
 					return { false, nullptr };
 				}
 			}
 			bucket.first[bucket.second].first = value;
 			bucket.first[bucket.second].second = true;
-			Element* element = *_elements.pushBack(&bucket.first[bucket.second++]);
+			Element* element = *_elements.PushBack(&bucket.first[bucket.second++]);
 			if ((float)_elements.size() / _capacity >= 0.8f) {
-				reserve(_capacity * 2);
+				Reserve(_capacity * 2);
 			}
 			return { true, &element->first } ;
 		}
 
 		template<typename... Args>
-		inline Tuple<bool, T*> emplace(Args&&... args) {
-			if (_trash > 8) {
-				cleanup();
-			}
+		inline Tuple<bool, T*> Emplace(Args&&... args) {
 			T value(std::forward<Args>(args)...);
 			uint64_t hash = Hasher()(value);
 			Bucket& bucket = _buckets[hash % _capacity];
@@ -129,27 +139,30 @@ namespace simple {
 						return { false, nullptr };
 					}
 				}
-				simple_engine::logMessage("bad hash!");
+				simple::logMessage(this, "bad hash (function simple::Set::Emplace)!");
 				if (bucket.second >= MaxBucketSize - 1) {
 					return { false, nullptr };
 				}
 			}
-			getValueAllocator().construct(&bucket.first[bucket.second].first, std::move(value));
+			new(&bucket.first[bucket.second].first) (std::move(value));
 			bucket.first[bucket.second].second = true;
-			_elements.pushBack(&bucket.first[bucket.second]);
+			_elements.PushBack(&bucket.first[bucket.second]);
 			if ((float)_elements.size() / _capacity >= 0.8f) {
-				reserve(_capacity * 2);
+				Reserve(_capacity * 2);
 			}
 			return { true, &bucket.first[bucket.second++].first };
 		}
 
-		inline bool erase(const T& value) {
+		inline bool Erase(const T& value) {
+			if (_trash > 8) {
+				Cleanup();
+			}
 			uint64_t hash = Hasher()(value);
 			auto& bucket = _buckets[hash % _capacity];
 			if (bucket.second) {
 				for (Element& element : bucket.first) {
 					if (element.first == value) {
-						getValueAllocator().destroy(&element.first);
+						&element.first->~T();
 						element.second = false;
 						bucket.second--;
 						_trash++;
@@ -160,7 +173,7 @@ namespace simple {
 			return false;
 		}
 
-		inline bool contains(const T& value) {
+		inline bool Contains(const T& value) {
 			uint64_t hash = Hasher()(value);
 			auto& bucket = _buckets[hash % _capacity];
 			if (bucket.second == 0) {
@@ -174,7 +187,7 @@ namespace simple {
 			return false;
 		}
 
-		inline const T* find(const T& value) {
+		inline const T* Find(const T& value) {
 			uint64_t hash = Hasher()(value);
 			auto& bucket = _buckets[hash % _capacity];
 			if (bucket.second == 0) {
@@ -188,37 +201,13 @@ namespace simple {
 			return nullptr;
 		}
 
-		size_t size() {
+		size_t Size() {
 			return _elements.size() - _trash;
 		}
 
-		void reserve(uint32_t capacity) {
-			if (capacity <= _capacity) {
-				return;
-			}
-			uint32_t oldCapacity = _capacity;
-			while (capacity > _capacity) {
-				_capacity *= 2;
-			}
-			Bucket* temp = _buckets;
-			_buckets = getBucketAllocator().allocate(_capacity);
-			for (size_t i = 0; i < _capacity; i++) {
-				getBucketAllocator().construct(&_buckets[i]);
-			}
-			_elements.clear();
-			_elements.reserve(_capacity);
-			for (size_t i = 0; i < oldCapacity; i++) {
-				for (size_t j = 0; j < temp[i].second; j++) {
-					emplace(std::move(temp[i].first[j].first));
-				}
-			}
-			getBucketAllocator().deallocate(temp, 1);
-		}
-
-
 		inline Iterator begin() {
 			Element** ptr = _elements.begin();
-			if (!ptr || !_elements.size()) {
+			if (!ptr || !_elements.Size()) {
 				return Iterator(ptr, _elements);
 			}
 			while (ptr != _elements.end() && !(*ptr)->second) {
@@ -233,9 +222,9 @@ namespace simple {
 
 		~Set() {
 			for (T& value : *this) {
-				getValueAllocator().destroy(&value);
+				&value->~T();
 			}
-			getBucketAllocator().deallocate(_buckets, 1);
+			Allocator().deallocate(_buckets, 1);
 			_buckets = nullptr;
 		}
 
