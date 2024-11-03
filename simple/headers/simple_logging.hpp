1925f2dad2acd50237ceeb7cebed4390ceef45c8
Ëinari Mäkiranta <makirantaeinari@gmail.com>
2024-11-03 22:37:35 +0200

    Added simple:logMessage function

--- simple/headers/simple_logging.hpp
+++ simple/headers/simple_logging.hpp
@@ -3,7 +3,12 @@
 #include <iostream>
 
 namespace simple {
+
 	static inline void logError(void* object, const char* err) noexcept {
-		std::cerr << "Error thrown by object at memory location " << object << ": " << err;
+		std::cerr << "Error thrown by object at address " << object << ": " << err;
 	};
+
+	static inline void logMessage(void* object, const char* message) noexcept {
+		std::clog << "Message from object at address " << object << ": " << message;
+	}
 }
