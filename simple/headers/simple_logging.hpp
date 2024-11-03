8d5712052bc5e7446d6dfbd4df7143d85187844a
Ëinari Mäkiranta <makirantaeinari@gmail.com>
2024-11-04 00:10:29 +0200

    Modified logging functions

--- simple/headers/simple_logging.hpp
+++ simple/headers/simple_logging.hpp
@@ -4,11 +4,11 @@
 
 namespace simple {
 
-	static inline void logError(void* object, const char* err) noexcept {
+	static inline void logError(const void* object, const char* err) noexcept {
 		std::cerr << "Error thrown by object at address " << object << ": " << err;
 	};
 
-	static inline void logMessage(void* object, const char* message) noexcept {
+	static inline void logMessage(const void* object, const char* message) noexcept {
 		std::clog << "Message from object at address " << object << ": " << message;
 	}
 }
