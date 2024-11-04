2e95ed06f34d01c748ae64fae656dd2d35e50257
Ëinari Mäkiranta <makirantaeinari@gmail.com>
2024-11-04 21:42:35 +0200

    Added simple::vulkan::FindSupportedFormat function

--- simple/headers/simple_vulkan.hpp
+++ simple/headers/simple_vulkan.hpp
@@ -76,5 +76,28 @@ namespace simple {
 				}
 			}
 		};
+
+		template<size_t candidate_count>
+		inline VkFormat FindSupportedFormat(VkPhysicalDevice vkPhysicalDevice, const VkFormat candidateFormats[candidate_count], VkImageTiling vkImageTiling, VkFormatFeatureFlags vkFormatFeatureFlags) {
+			for (size_t i = 0; i < candidate_count; i++) {
+				VkFormatProperties formatProperties;
+				vkGetPhysicalDeviceFormatProperties(vkPhysicalDevice, candidateFormats[i], &formatProperties);
+				switch (vkImageTiling) {
+					case VK_IMAGE_TILING_OPTIMAL:
+						if ((formatProperties.optimalTilingFeatures & vkFormatFeatureFlags) == vkFormatFeatureFlags) {
+							return candidateFormats[i];
+						}
+						break;
+					case VK_IMAGE_TILING_LINEAR:
+						if ((formatProperties.linearTilingFeatures & vkFormatFeatureFlags) == vkFormatFeatureFlags) {
+							return candidateFormats[i];
+						}
+						break;
+					default:
+						break;
+				}
+			}
+			return VK_FORMAT_UNDEFINED;
+		};
 	}
 }
