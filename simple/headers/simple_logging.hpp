7b8930ed10e55f5383ad5e3cbde4b45a90e2e244
Ëinari Mäkiranta <makirantaeinari@gmail.com>
2024-11-04 20:51:06 +0200

    Added simple::logWarning function

--- simple/headers/simple_logging.hpp
+++ simple/headers/simple_logging.hpp
@@ -4,11 +4,15 @@
 
 namespace simple {
 
-	static inline void logError(const void* object, const char* err) noexcept {
-		std::cerr << "Error thrown by object at address " << object << ": " << err;
-	};
-
 	static inline void logMessage(const void* object, const char* message) noexcept {
-		std::clog << "Message from object at address " << object << ": " << message;
+		std::clog << "Message from object at address " << object << ": " << message << "\n";
 	}
+
+	static inline void logWarning(const void *object, const char *warn) noexcept {
+		std::clog << "Warning from object at address " << object << ": " << warn << "\n";
+	}
+
+	static inline void logError(const void* object, const char* err) noexcept {
+		std::cerr << "Error thrown by object at address " << object << ": " << err << "\n";
+	};
 }
