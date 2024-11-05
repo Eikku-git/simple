5258a5255f53c7e56228986634d292d960e66d31
Ëinari Mäkiranta <makirantaeinari@gmail.com>
2024-11-05 19:18:04 +0200

    Test main.cpp function

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,8 +1,10 @@
 #include "simple.hpp"
 
 int main() {
+	simple::WindowSystem::Init();
 	simple::Window window{};
 	window.Init(540, 540, "Test", nullptr);
 	simple::Simple engine(std::move(window));
+	simple::WindowSystem::Terminate();
 	return 0;
 }
